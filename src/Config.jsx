e0886747e444d027060598bf1c70fd08f5e5284c
Appsurfs_DEV <dev@appsurfs.com>
2022-02-13 15:00:47 +0800

    reorder config

--- src/Config.jsx
+++ src/Config.jsx
@@ -1,20 +1,3 @@
-export const OptionList = [
-    { displayName: "Preferred Day", schemaName: "preferredappointmentdaycode" },
-    { displayName: "Payment Terms", schemaName: "paymenttermscode" },
-    { displayName: "Shipping Method", schemaName: "address1_shippingmethodcode" }
-]
-
-export const TableColumns = [
-    { title: 'Full Name', field: 'fullname' },
-    { title: 'Email', field: 'emailaddress1' }
-]
-
-export const TextFieldList = [
-    { displayName: "Name", schemaName: "fullname" },
-    { displayName: "Email", schemaName: "emailaddress1" },
-    { displayName: "Job Title", schemaName: "jobtitle" }
-]
-
 export const fieldsConfig = [
     { displayName: "Name", schemaName: "fullname", type: "string", showType: "text" },
     { displayName: "Email", schemaName: "emailaddress1", type: "string", showType: "text" },
@@ -26,6 +9,11 @@ export const fieldsConfig = [
         displayName: "", schemaName: "", type: "lookup", showType: "multiselect", 
         lookupConfig: { entityName: "", primaryName: "", primaryID: "", }
     }
+]
+
+export const TableColumns = [
+    { title: 'Full Name', field: 'fullname' },
+    { title: 'Email', field: 'emailaddress1' }
 ]
 
 export const EntityName = "contact"
